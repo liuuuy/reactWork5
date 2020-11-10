2fcafa2588e77ebefd46256719d67f5f67229d5f
DyingA <971258403@qq.com>
2020-11-10 15:53:37 +0800

    ok

---
+++ zuoye/precache-manifest.3c0d604459cb9b322dbac820b7f5da55.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2ca8f207dffdc5efa4ce98dfce18698e",
+    "url": "/index.html"
+  },
+  {
+    "revision": "8095e30fbf38a8d7c255",
+    "url": "/static/css/2.f98a1de1.chunk.css"
+  },
+  {
+    "revision": "01358cad0668025a91f4",
+    "url": "/static/css/main.6d5aa447.chunk.css"
+  },
+  {
+    "revision": "8095e30fbf38a8d7c255",
+    "url": "/static/js/2.e67d9d74.chunk.js"
+  },
+  {
+    "revision": "4d47ffde3dc69784e8e23fd910817ef8",
+    "url": "/static/js/2.e67d9d74.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "01358cad0668025a91f4",
+    "url": "/static/js/main.6052efa5.chunk.js"
+  },
+  {
+    "revision": "e2702865a21868f2ebe6",
+    "url": "/static/js/runtime-main.7918a0be.js"
+  }
+]);
